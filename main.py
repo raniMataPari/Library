f8c72bfe25ff234b8a216ef2088bbba4b29f7c8f
Sushma Shukla <sushma.shukla3011@gmail.com>
2026-01-11 22:04:24 +0530

    Added login feature

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 print("Hello,this is library management system")
 #Team:Sushma,Apurva 
+
 print("Student Registration Module Loaded")
 print("New student can be registered")
+
+print("Login Module Loaded")
+print("User can login with username and password")
+
+
